5f9927cc7495a2b7d2660134a12da99bff154b94
Jair Partida <jairpartida1@gmail.com>
2022-10-02 19:54:28 -0600

    Form component ui done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Form = () => {
+    const [search, setSearch] = useState({
+        artist: '',
+        song: ''
+    })
+
   return (
-    <div>Form</div>
+    <form>
+        <legend>Search by Artist and Song</legend>
+
+        <div className="form-grid">
+            <div>
+                <label>Artist</label>
+                <input 
+                    type="text"
+                    name="artist"
+                    placeholder="Artist Name"
+                    value={search.artist}
+                    onChange={e => setSearch({
+                        ...search,
+                        [e.target.name] : e.target.value
+                    })}
+                />
+            </div>
+
+            <div>
+                <label>Song</label>
+                <input 
+                    type="text"
+                    name="song"
+                    placeholder="Song Name"
+                    value={search.song}
+                    onChange={e => setSearch({
+                        ...search,
+                        [e.target.name] : e.target.value
+                    })}
+                />
+            </div>
+
+            <input type="submit" value="Search"/>
+        </div>
+    </form>
   )
 }
 
