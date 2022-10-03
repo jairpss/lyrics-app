f6f21b2843f9f56af730291f8d475f43ee1e9fc4
Jair Partida <jairpartida1@gmail.com>
2022-10-03 16:42:13 -0600

    creating provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import AppLyrics from './components/AppLyrics'
+import {LyricsProvider} from './context/LyricsProvider'
 
 function App() {
 
   return (
-    <div className="App">
+    <LyricsProvider>
         <AppLyrics />
-    </div>
+    </LyricsProvider>
   )
 }
 
---
+++ src/context/LyricsProvider.jsx
@@ -0,0 +1,19 @@
+import {useState, createContext} from 'react' 
+
+const LyricsContext = createContext() 
+
+const LyricsProvider = ({children}) => {
+    return (
+        <LyricsContext.Provider
+            value={{}}
+        >
+            {children}
+        </LyricsContext.Provider> 
+    )
+}
+
+export { 
+    LyricsProvider 
+}
+
+export default  LyricsContext
